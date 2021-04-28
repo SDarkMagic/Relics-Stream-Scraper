d8935c17d72e3dd8110fbb2a25ea61cf461f6e80
SDarkMagic <60792071+SDarkMagic@users.noreply.github.com>
2021-04-27 22:08:21 -0400

    API stuffs and things

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 import Lib.API as api
+import json
 
 botwGameId = 110758
 
-def checkStreams(pagination, maxLen):
+def checkStreams(pagination=None, maxLength=50, resultList=[]):
+    print('maxLength ', maxLength)
     streams = api.getStreams(botwGameId, maxLen=maxLength, pagination=pagination)
+    resultList.extend(streams['data'])
+    if len(streams['data']) == maxLength or len(streams['data']) == maxLength:
+        print('if')
+        resultList.extend(checkStreams(pagination=streams['pagination']['cursor'], maxLength=maxLength, resultList=resultList))
+    else:
+        print('else')
+        pass
+    return(resultList)
 
 def main():
     streamList = []
@@ -11,3 +21,9 @@ def main():
 
     streamList.append(initialStreams['data'])
     if len(initialStreams['data'] == maxLength):
+        pass
+
+with open('response.json', 'wt') as writeData:
+    status = checkStreams(maxLength=100)
+    print(len(status))
+    writeData.write(json.dumps(status, indent=2))

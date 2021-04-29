60b14ebc15b5c49aed6fbb122599f70cdaac48c7
dragonbuilderx <>
2021-04-29 02:09:23 +0000

    idk, it does something

--- main.py
+++ main.py
@@ -1,29 +1,37 @@
 import Lib.API as api
 import json
+import asyncio
 
 botwGameId = 110758
-
-def checkStreams(pagination=None, maxLength=50, resultList=[]):
+async def checkStreams(pagination=None, maxLength=50, resultList=[]):
     print('maxLength ', maxLength)
     streams = api.getStreams(botwGameId, maxLen=maxLength, pagination=pagination)
     resultList.extend(streams['data'])
     if len(streams['data']) == maxLength or len(streams['data']) == maxLength:
         print('if')
-        resultList.extend(checkStreams(pagination=streams['pagination']['cursor'], maxLength=maxLength, resultList=resultList))
+        resultList.extend(await checkStreams(pagination=streams['pagination']['cursor'], maxLength=maxLength, resultList=resultList))
     else:
         print('else')
         pass
     return(resultList)
 
-def main():
-    streamList = []
+async def main():
     maxLength = 100
+    relicsStreams = []
+    streams = await checkStreams(maxLength=maxLength)
+    
+    for stream in streams:
+      if isinstance(stream, dict):
+        title = stream['title'].lower().split(' ')
+        #print(' '.join(title))
+        if 'relics' in title and stream['type'] == 'live':
+          print(title)
+          relicsStreams.append(stream)
 
-    streamList.append(initialStreams['data'])
-    if len(initialStreams['data'] == maxLength):
-        pass
 
-with open('response.json', 'wt') as writeData:
-    status = checkStreams(maxLength=100)
-    print(len(status))
-    writeData.write(json.dumps(status, indent=2))
+    with open('response.json', 'wt') as writeData:
+      status = streams
+      print(len(status))
+      writeData.write(json.dumps(relicsStreams, indent=2))
+      
+asyncio.run(main())
